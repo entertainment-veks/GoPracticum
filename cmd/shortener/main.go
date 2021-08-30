85b4bc3dd0df8dd3fc9c2bd02f7cc3cc69ff0488
egorka <entertainment.veks@gmail.com>
2021-08-30 21:33:58 +0300

    create cache

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
@@ -11,34 +10,6 @@ import (
 )
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-var database map[string]string
-
-func PostMethod(w http.ResponseWriter, r *http.Request) {
-	body, _ := ioutil.ReadAll(r.Body)
-
-	link := string(body)
-
-	if !isURL(link) {
-		w.WriteHeader(400)
-		return
-	}
-
-	var code = generateCode()
-
-	database[code] = link
-
-	w.Write([]byte("http://localhost:8080/" + code))
-	w.WriteHeader(201)
-}
-
-func GetMethod(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	code := database[vars["key"]]
-
-	w.Header().Set("Location", code)
-	w.WriteHeader(307)
-}
 
 func generateCode() string {
 	b := make([]byte, 5)
@@ -57,8 +28,41 @@ func isURL(token string) bool {
 }
 
 func main() {
+	database := make(map[string]string)
+
 	router := mux.NewRouter()
-	router.HandleFunc("/", PostMethod)
-	router.HandleFunc("/{key}", GetMethod)
-	fmt.Println(http.ListenAndServe(":8080", router))
+
+	router.HandleFunc("/{key}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		code := database[vars["key"]]
+
+		w.Header().Set("Location", code)
+		w.WriteHeader(307)
+	})
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		temp := r.Body
+
+		body, err := ioutil.ReadAll(temp)
+
+		if err != nil {
+			panic(err)
+		}
+
+		link := string(body)
+
+		if !isURL(link) {
+			w.WriteHeader(400)
+			return
+		}
+
+		var code = generateCode()
+
+		database[code] = link
+
+		w.Write([]byte("http://localhost:8080/" + code))
+		w.WriteHeader(201)
+	})
+
+	http.ListenAndServe(":8080", router)
 }
