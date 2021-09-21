8f83ac2c363e8f9588432c43262a801981f73a3b
entertainment-veks <entertainment.veks@gmail.com>
2021-09-21 19:31:11 +0300

    add command line configuration

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 	"os"
 
@@ -9,6 +10,25 @@ import (
 
 	"github.com/gorilla/mux"
 )
+
+func init() {
+	flag.Func("a", "Server address", func(s string) error {
+		os.Setenv("SERVER_ADDRESS", s)
+		return nil
+	})
+
+	flag.Func("b", "Base url", func(s string) error {
+		os.Setenv("BASE_URL", s)
+		return nil
+	})
+
+	flag.Func("f", "File storage path", func(s string) error {
+		os.Setenv("FILE_STORAGE_PATH", s)
+		return nil
+	})
+
+	flag.Parse()
+}
 
 func SetupServer() mux.Router {
 
@@ -28,16 +48,16 @@ func SetupServer() mux.Router {
 
 func main() {
 
-	if len(os.Getenv("FILE_STORAGE_PATH")) == 0 {
-		os.Setenv("FILE_STORAGE_PATH", "file")
-	}
-
 	if len(os.Getenv("SERVER_ADDRESS")) == 0 {
 		os.Setenv("SERVER_ADDRESS", ":8080")
 	}
 
 	if len(os.Getenv("BASE_URL")) == 0 {
 		os.Setenv("BASE_URL", "http://localhost:8080")
+	}
+
+	if len(os.Getenv("FILE_STORAGE_PATH")) == 0 {
+		os.Setenv("FILE_STORAGE_PATH", "file")
 	}
 
 	router := SetupServer()
