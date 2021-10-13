22451e887862a3636a05edc08385b2fb49956c69
entertainment-veks <entertainment.veks@gmail.com>
2021-10-13 18:04:08 +0300

    experiments with tests

--- internal/app/shortener/shortener.go
+++ internal/app/shortener/shortener.go
@@ -2,20 +2,20 @@ package shortener
 
 import (
 	"database/sql"
-	"go_practicum/internal/app/store/sqlstore"
+	"go_practicum/internal/app/store/teststore"
 	"net/http"
 )
 
 const driverName = "postgres"
 
 func Start(config *Config) error {
-	db, err := newDB(config.DatabaseURL)
-	if err != nil {
-		return err
-	}
+	//db, err := newDB(config.DatabaseURL)
+	// if err != nil {
+	// 	return err
+	// }
 
-	defer db.Close()
-	store := sqlstore.New(db)
+	// defer db.Close()
+	store := teststore.New()
 	s := newServer(store, config.BaseURL)
 
 	return http.ListenAndServe(config.ServerAddress, s)
