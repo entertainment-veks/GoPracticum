d865595ffc5d99f99cdf2d90d80c58cd52f5f0bf
egorka <y.sereda@bit-flow.io>
2022-03-10 12:32:02 +0200

    15th increment

--- app/model/link.go
+++ app/model/link.go
@@ -2,7 +2,6 @@ package model
 
 import (
 	validation "github.com/go-ozzo/ozzo-validation"
-	"github.com/go-ozzo/ozzo-validation/is"
 	"regexp"
 )
 
@@ -14,14 +13,14 @@ type Link struct {
 }
 
 func (l *Link) Validate() error {
-	regex, err := regexp.Compile("^(http).*")
+	regex, err := regexp.Compile("^(http(s)?://)[\\w.-]+(?:\\.[\\w.-]+)+[\\w\\-._~:/?#[\\]@!$&'()*+,;=]+$")
 	if err != nil {
 		return err
 	}
 
 	return validation.ValidateStruct(
 		l,
-		validation.Field(&l.Link, validation.Required, is.URL, validation.Match(regex)),
+		validation.Field(&l.Link, validation.Required, validation.Match(regex)),
 		validation.Field(&l.Code, validation.Required),
 		validation.Field(&l.UserID, validation.Required),
 	)
