73745273dda5445d2001da1965f59d8f8a73f261
egorka <y.sereda@bit-flow.io>
2022-03-28 13:43:50 +0300

    global vars

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"go_practicum/app/config"
 	"go_practicum/app/shortener"
 	"log"
 )
 
+var (
+	buildVersion string = "N/A"
+	buildDate    string = "N/A"
+	buildCommit  string = "N/A"
+)
+
 func main() {
+	typeGlVars()
+
 	cfg := config.NewConfig()
 
 	if err := shortener.Start(*cfg); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func typeGlVars() {
+	fmt.Printf("Build version: %v\n", buildVersion)
+	fmt.Printf("Build date: %v\n", buildDate)
+	fmt.Printf("Build commit: %v\n", buildCommit)
 }
